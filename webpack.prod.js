8fb9a9baf997aee13723be8de56ff417c68ae51b
Gengyuan Huang <gengyuan@ualberta.ca>
2022-06-11 14:19:52 -0600

    Update config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
